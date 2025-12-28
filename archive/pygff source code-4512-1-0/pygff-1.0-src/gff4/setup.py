54bb794c8b03c8714b798d389fbbc038366512ed
mwakeman1 <49767275+mwakeman1@users.noreply.github.com>
2025-12-28 10:29:06 +0900

    removed archive

--- archive/pygff source code-4512-1-0/pygff-1.0-src/gff4/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name='pygff',
-    version='0.2',
-    description='Python Generic File Format Tools',
-    author='William Clark',
-    author_email='wsclark3@alumni.umass.edu',
-)
