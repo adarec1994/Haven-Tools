dc69deedfdc4b82214c4c8f768f8f2d55dbea8dc
Matthew <49767275+adarec1994@users.noreply.github.com>
2026-06-01 04:43:03 +0900

    removed wiki

--- Wiki/run_wiki.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-$scriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
-$port = if ($args.Count -gt 0) { [int]$args[0] } else { 8765 }
-python (Join-Path $scriptDir "serve.py") --port $port
